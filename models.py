35ad6a2cfebf46f99a3a7156e02001e639da5997
Aman Ak <amanmaxiz@gmail.com>
2023-02-27 11:14:38 +0530

    created todolist with flask

---
+++ models.py
@@ -0,0 +1,13 @@
+from app import *
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Task %r>' % self.id
+
+    def init_db():
+        db.create_all()
